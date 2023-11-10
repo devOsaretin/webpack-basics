882baddae76d237b2e267ea3bee21780e64858e6
osaretini <osaretini@apadmi.com>
2023-11-10 14:02:21 +0000

    feat: add asset loader

---
+++ 5.ImageLoaders/src/index.js
@@ -0,0 +1,25 @@
+import _ from "lodash";
+import style from "./index.scss";
+import "./clearButton";
+import logo from "./assests/webpacklogo.png";
+
+console.log(style);
+const btn1 = document.getElementById("btn1");
+
+const logoEl = document.getElementById("logo");
+
+btn1.addEventListener("click", function () {
+	const el = document.getElementById("header");
+	el.innerHTML = "The content has been updated!";
+
+	const listItems = ["Apple", "Orange", "Banana"];
+	const ul = document.getElementById("list");
+	_.forEach(listItems, function (item) {
+		const tempEl = document.createElement("li");
+		tempEl.innerHTML = item;
+		ul.appendChild(tempEl);
+	});
+});
+
+btn1.classList.add([style.button]);
+logoEl.src = logo;
